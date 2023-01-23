0fa97f468da1e60e7f3750626e446e4691a62e4a
Raka Adriel <114383364+adrielrakaa@users.noreply.github.com>
2023-01-23 23:55:04 +0700

    Add files via upload

---
+++ Percobaan2/ExceptionError.java
@@ -0,0 +1,17 @@
+package Sesi11.Percobaan2;
+
+public class ExceptionError {
+    public static void main(String[] args) {
+        int i=0;
+        String greeting[]={
+            "Hello World!",
+            "No, I mean it!",
+            "Hello World"
+        };
+        while(i<4)
+        {
+            System.out.println(greeting[i]);
+            i++;
+        }
+    }
+}
