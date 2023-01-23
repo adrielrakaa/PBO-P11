62abebc1976964d3df25060e399d492c416bc05a
Raka Adriel <114383364+adrielrakaa@users.noreply.github.com>
2023-01-23 23:34:22 +0700

    Add files via upload

---
+++ Percobaan1/ExceptionError.java
@@ -0,0 +1,8 @@
+package Sesi11.Percobaan1;
+
+public class ExceptionError {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        a[5]=100;
+    }
+}
---
+++ Percobaan1/ExceptionFix.java
@@ -0,0 +1,12 @@
+package Sesi11.Percobaan1;
+
+public class ExceptionFix {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+        try {
+            a[5]=100;
+        } catch (Exception e) {
+            System.out.println("Terjadi pelanggaran memory");
+        }
+    }
+}
