f69c6117b8ee53ea04ca64798b435f682c3d0546
Raka Adriel <114383364+adrielrakaa@users.noreply.github.com>
2023-01-24 00:09:43 +0700

    Add files via upload

---
+++ Percobaan8/ThrowsError.java
@@ -0,0 +1,21 @@
+package Sesi11.Percobaan8;
+
+import java.io.IOException;
+
+public class ThrowsError {
+    public void methodA(){
+        System.out.println("Method A");
+    }
+    public void methodB() throws IOException {
+        System.out.println(20/0);
+        System.out.println("Method B");
+    }
+}
+class Utama
+{
+    public static void main(String[] args) throws IOException {
+        ThrowsError c=new ThrowsError();
+        c.methodA();
+        c.methodB();
+    }
+}
