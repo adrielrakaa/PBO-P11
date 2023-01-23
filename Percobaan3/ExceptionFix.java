c616c4549982c889631fec9723543619e8ec5561
Raka Adriel <114383364+adrielrakaa@users.noreply.github.com>
2023-01-23 23:56:28 +0700

    Add files via upload

---
+++ Percobaan3/ExceptionFix.java
@@ -0,0 +1,12 @@
+package Sesi11.Percobaan3;
+
+public class ExceptionFix {
+    public static void main(String[] args) {
+        int bil=10;
+        try {
+            System.out.println(bil/0);
+        } catch (Exception e) {
+            System.out.println("Ini menghandle error yang terjadi");
+        }
+    }
+}
